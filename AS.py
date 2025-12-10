955ba705fd9f5791d4c0006d27e048f01a33aaea
leeglb <galbraithlee76@gmail.com>
2025-12-10 20:13:08 +1000

    activation system + notebook

--- AS.py
+++ AS.py
@@ -4,16 +4,25 @@ from ultralytics import YOLO
 from config import API_KEY
 from config import USER_EMAIL
 from courier import Courier
+from datetime import datetime 
+import time 
+
 
 # Global Variables -------------------------
 
 client = Courier(api_key = API_KEY)
 
-print(API_KEY)
-
 model = YOLO("yolo11n.pt") #pretrained model 
 
 video_path = cv2.VideoCapture(0)
+
+people_counter = 0 
+
+alarm_started = False
+
+DURATION = 10
+
+countdown = 0.0
 
 # ------------------------------------------
 
@@ -30,7 +39,7 @@ class LiveFeed():
 
             "content": { 
                 "title": "Home Surveillance Alert",
-                "body": "The System Has Detected Intruders"
+                "body": f"The System Has Detected {people_counter} Intruders"
             },
 
             "routing": {
@@ -39,19 +48,12 @@ class LiveFeed():
             }
             }
         )
-        print(response.request_id)
-        print(response)
-
-
-
-    def activate_system(self):
-
-
-        pass
 
     def main_function(self):
 
         while(True): 
+            
+            current_datetime = datetime.now()
             
             boolean_ret, capture_frame = video_path.read()
             
@@ -59,7 +61,30 @@ class LiveFeed():
                 
                 results = model.track(capture_frame, persist=True, classes=0) #classes = 0 for just people tracking. 
                 
+                people_counter = len(results[0].boxes) #counts number of boxes identified
+                
                 annotated_frame = results[0].plot() 
+                
+                global alarm_started
+                global countdown
+                 
+                if alarm_started: 
+
+                    time_remaining = int(countdown - time.time())
+
+                    if time_remaining >= 0: 
+
+                        cv2.putText(annotated_frame, f"Countdown To Alarm Activation: {time_remaining}", (20, 300), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 1)
+                        
+                        if time_remaining <= 0: 
+                            
+                            if people_counter >= 1:
+                                
+                                self.email_system()
+                
+                cv2.putText(annotated_frame, str(current_datetime), (20, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+                
+                cv2.putText(annotated_frame, f"Number of individuals detected: {people_counter}", (20, 600), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
                 
                 cv2.imshow("Monitoring System", annotated_frame)
 
@@ -69,10 +94,13 @@ class LiveFeed():
                     
                     break
 
-                elif key == ord("s"):
+                elif key == ord("s"): #start alarm 
+                    
+                    alarm_started = True
+                    
+                    countdown = time.time() + DURATION
 
-                    self.email_system()
-                
+                                        
             else:
                 
                 break
@@ -80,6 +108,8 @@ class LiveFeed():
         
         video_path.release()
         cv2.destroyAllWindows()
+        
+
 
 
 # Running functions. 
