6a940c6ef5cfe7481b9547e5bcf02642ed8e199d
leeglb <galbraithlee76@gmail.com>
2026-01-20 22:55:05 +1000

    refactored into self. same issue with captured videos though. Check email system is working tomorrow too

--- AS.py
+++ AS.py
@@ -1,5 +1,3 @@
-
-
 import cv2
 
 from ultralytics import YOLO
@@ -12,96 +10,84 @@ import time
 
 
 
-
-
-# API Key & ML Model -------------------------
-
-client = Courier(api_key = API_KEY)
-
-model = YOLO("yolo11n.pt") #pretrained model 
-
-
-
-
-# Video Path & Parameters --------------------
-
-video_path = cv2.VideoCapture(0)
-
-video_path.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-video_path.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-frameWidth = int(video_path.get(cv2.CAP_PROP_FRAME_WIDTH))
-frameHeight = int(video_path.get(cv2.CAP_PROP_FRAME_HEIGHT))
-frameRate =  (video_path.get(cv2.CAP_PROP_FPS))
-
-fourccCode = cv2.VideoWriter_fourcc(*'mp4v')
-
-recordedVideo = None 
-
-videoDimensions = (frameWidth, frameHeight)
-videoFileName = ""
-
-
-
-
-
-# Buffer Queue Parameters ------------------
-
-BUFFER_SECONDS = 15
-POST_EVENT_SECONDS = 15 #always equal 
-
-BUFFER_SIZE = BUFFER_SECONDS * int(frameRate)
-POST_EVENT_SIZE = BUFFER_SIZE # equal too 
-
-frame_buffer = deque(maxlen=BUFFER_SIZE)
-
-
-
-
-# Timers & Countdowns ---------------------
-
-system_recording = False
-
-intruder_count = 0
-
-intrusion_time = 0.0 
-
-end_time = 0.0 
-
-DURATION = 10
-
-EMAIL_COOLDOWN = 10  # subject to change depending on real world application (i.e 150-300)
-
-countdown = 0
-
-post_event_countdown = 0
-
-post_event_flag = False 
-post_event_timer = 0.0
-
-previous_intruder_count = 0 
-
-
-
-# System Boolean | Window Name | Final Frame 
-
-result_frame = None
-
-system_activated = False 
-
-first_presence = False # First presence detected. --> Kicks the system into alert. 
-
-email_sent = False
-
-WINDOW_NAME = "Monitoring System" 
-
-# ------------------------------------------
-
-
 class LiveFeed():
     
-    def __init__(self) -> None:
-         pass # for now
+    def __init__(self):
+        
+        # API Key & ML Model -------------------------
+
+        self.client = Courier(api_key = API_KEY)
+
+        self.model = YOLO("yolo11n.pt") #pretrained model 
+
+        # Video Path & Parameters --------------------
+
+        self.video_path = cv2.VideoCapture(0)
+
+        self.video_path.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.video_path.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+        self.frameWidth = int(self.video_path.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frameHeight = int(self.video_path.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.frameRate =  (self.video_path.get(cv2.CAP_PROP_FPS))
+
+        self.fourccCode = cv2.VideoWriter_fourcc(*'mp4v')
+
+        self.recordedVideo = None 
+
+        self.videoDimensions = (self.frameWidth, self.frameHeight)
+        self.videoFileName = ""
+
+
+        
+        # Buffer Queue Parameters ------------------
+
+        self.BUFFER_SECONDS = 15
+        self.POST_EVENT_SECONDS = 15 #always equal 
+
+        self.BUFFER_SIZE = self.BUFFER_SECONDS * int(self.frameRate)
+        self.POST_EVENT_SIZE = self.BUFFER_SIZE # equal too 
+
+        self.frame_buffer = deque(maxlen=self.BUFFER_SIZE)
+
+
+        # Timers & Countdowns ---------------------
+
+        self.system_recording = False
+
+        self.intruder_count = 0
+
+        self.intrusion_time = 0.0 
+
+        self.end_time = 0.0 
+
+        self.DURATION = 10
+
+        self.EMAIL_COOLDOWN = 10  # subject to change depending on real world application (i.e 150-300)
+
+        self.countdown = 0
+
+        self.post_event_countdown = 0
+
+        self.post_event_flag = False 
+        self.post_event_timer = 0.0
+
+        self.previous_intruder_count = 0 
+
+        
+        # System Boolean | Window Name | Final Frame 
+
+        self.result_frame = None
+
+        self.system_activated = False 
+
+        self.first_presence = False # First presence detected. --> Kicks the system into alert. 
+
+        self.email_sent = False
+
+        self.WINDOW_NAME = "Monitoring System" 
+
+        # ------------------------------------------
 
     def email_system(self):
         
@@ -109,9 +95,9 @@ class LiveFeed():
             
         current_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
 
-        intruder_plural = "Intruder" if intruder_count == 1 else "Intruders"
+        intruder_plural = "Intruder" if self.intruder_count == 1 else "Intruders"
 
-        response = client.send.message(
+        response = self.client.send.message(
             message = { 
             "to": { 
                 "email": USER_EMAIL
@@ -119,7 +105,7 @@ class LiveFeed():
 
             "content": { 
                 "title": "Home Surveillance Alert",
-                "body": f"The System Has Detected {intruder_count} {intruder_plural} At {current_datetime}"
+                "body": f"The System Has Detected {self.intruder_count} {intruder_plural} At {current_datetime}"
             },
 
             "routing": {
@@ -137,7 +123,7 @@ class LiveFeed():
             
             current_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
             
-            boolean_ret, capture_frame = video_path.read()
+            boolean_ret, capture_frame = self.video_path.read()
             
             if(boolean_ret): # if successful 
 
@@ -149,16 +135,11 @@ class LiveFeed():
 
                 """
 
-                global countdown
-                global system_activated
-                global result_frame
-                global videoFileName
+                self.result_frame = capture_frame # result frame is the final frame we will display. 
 
-                result_frame = capture_frame # result frame is the final frame we will display. 
+                if self.system_activated: 
 
-                if system_activated: 
-
-                    time_remaining = int(countdown - time.time())
+                    time_remaining = int(self.countdown - time.time())
 
                     if time_remaining >= 0: # system is now counting down towards activation.  
 
@@ -166,13 +147,13 @@ class LiveFeed():
 
                     if time_remaining < 0: # not <= since both cv2.putText will display at 0 seconds. 
                     
-                        results = model.track(capture_frame, persist=True, classes=0) #classes = 0 for just people tracking. 
+                        results = self.model.track(capture_frame, persist=True, classes=0) #classes = 0 for just people tracking. 
                         
                         annotated_frame = results[0].plot() 
 
-                        result_frame = annotated_frame
+                        self.result_frame = annotated_frame
 
-                        frame_buffer.append(annotated_frame)
+                        self.frame_buffer.append(annotated_frame)
 
                         cv2.putText(annotated_frame, "Monitoring System Is Now Activated", (20, 300), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2)
                         
@@ -185,29 +166,18 @@ class LiveFeed():
                         3. When the timer does exceed x seconds and intruders dissapear, the system will capture from 
                             when alarm is activated to x seconds after no intruders are detected.
                         """
-
-                        global intruder_count
-                        global system_recording
-                        global intrusion_time
-                        global recordedVideo
-                        global post_event_timer
-                        global email_sent
-                        global post_event_flag
-                        global post_event_countdown
-                        global previous_intruder_count
-                        global first_presence
             
-                        intruder_count = len(results[0].boxes) #counts number of boxes identified -> aka, number of intruders 
+                        self.intruder_count = len(results[0].boxes) #counts number of boxes identified -> aka, number of intruders 
                         
-                        cv2.putText(annotated_frame, f"Number of intruders detected: {intruder_count}", (20, 600), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+                        cv2.putText(annotated_frame, f"Number of intruders detected: {self.intruder_count}", (20, 600), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
 
-                        if (intruder_count > 0) and (system_recording == False): 
+                        if (self.intruder_count > 0) and (self.system_recording == False): 
 
-                            previous_intruder_count = intruder_count
+                            self.previous_intruder_count = self.intruder_count
 
-                            first_presence = True 
+                            self.first_presence = True 
                             
-                            post_event_flag = False 
+                            self.post_event_flag = False 
 
                             #intrusion_time = time.time()
 
@@ -217,18 +187,18 @@ class LiveFeed():
                             
                             #cv2.putText(annotated_frame, f"Intruders have been present for: {hours:02d}:{minutes:02d}:{seconds:02d}.", (20, 700), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
 
-                            videoFileName = f"Video_Recorded_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4"
+                            self.videoFileName = f"Video_Recorded_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4"
 
-                            recordedVideo = cv2.VideoWriter(videoFileName,
-                                    fourccCode,
-                                    frameRate,
-                                    videoDimensions) # this activates the video writer --> starts to capture. 
+                            self.recordedVideo = cv2.VideoWriter(self.videoFileName,
+                                    self.fourccCode,
+                                    self.frameRate,
+                                    self.videoDimensions) # this activates the video writer --> starts to capture. 
                             
-                            system_recording = True 
+                            self.system_recording = True 
                             
-                            for buffer_frame in frame_buffer: 
+                            for buffer_frame in self.frame_buffer: 
 
-                                recordedVideo.write(buffer_frame) # append the previous frames. 
+                                self.recordedVideo.write(buffer_frame) # append the previous frames. 
 
                             
                             # File Writing ---------
@@ -237,7 +207,7 @@ class LiveFeed():
                             
                             file_content = ""
                             
-                            if intruder_count == 1: 
+                            if self.intruder_count == 1: 
                                 
                                 file_content = f"1 Intruder Detected At {current_datetime}.\n"
                                 
@@ -245,85 +215,86 @@ class LiveFeed():
                                 
                                     file.write(file_content)
                                 
-                            elif intruder_count > 1: 
+                            elif self.intruder_count > 1: 
                             
-                                file_content = f"{intruder_count} Intruders Detected At {current_datetime}.\n"
+                                file_content = f"{self.intruder_count} Intruders Detected At {current_datetime}.\n"
                                 
                                 with open(file_path, 'a') as file: 
                                     
                                     file.write(file_content)
                                     
-                            # Email Sending -------
-                            
-                            """
-                            1.For our email system, we have already created the email function above the main function.
-                            
-                            2. The email system will activate only if an intruder is detected. As a result, we follow 
-                            the current if loop, allowing us to have a cooldown period. 
-                            
-                            3. If an intruder constantly appears, this will cause the system to spam ping the user.
-                            
-                            4. To avoid this, we allow the system to wait for the post even timer to finish.
-                            
-                            5. When the email is sent, we will also send a log to the logbook to record intrusion alerts. 
-                            
-                            """
-
-                            #self.email_system()
-
-                            email_sent = True 
-
-                            with open(file_path, 'a') as file: 
-                                
-                                    file.write(f"Alert Sent At {current_datetime}.\n")
                         
-                        intruder_not_present = intruder_count == 0 and previous_intruder_count > 0
-                        # intruder_currently_present = intruder_count > 0 and previous_intruder_count == 0
+                        intruder_not_present = self.intruder_count == 0 and self.previous_intruder_count > 0
+                        # intruder_currently_present = self.intruder_count > 0 and self.previous_intruder_count == 0
 
                         cv2.putText(annotated_frame, f"Intruders not present: " + str(intruder_not_present), (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-                        cv2.putText(annotated_frame, "Previous intruder count: " + str(previous_intruder_count), (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+                        cv2.putText(annotated_frame, "Previous intruder count: " + str(self.previous_intruder_count), (20, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
                                     
-                        if intruder_not_present and not post_event_flag:
+                        if intruder_not_present and not self.post_event_flag:
                             
-                            post_event_flag = True 
+                            self.post_event_flag = True 
                             
-                            post_event_timer = time.time()
+                            self.post_event_timer = time.time()
                             
                             cv2.putText(annotated_frame, f"Intruders are not present.", (20, 700), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
                             
-                        if post_event_flag:
+                        if self.post_event_flag:
                             
-                            recordedVideo.write(annotated_frame) 
+                            self.recordedVideo.write(annotated_frame) 
                             
-                            post_event_countdown = int(time.time() - post_event_timer)
+                            self.post_event_countdown = int(time.time() - self.post_event_timer)
                             
-                            time_since_dissappearance = POST_EVENT_SECONDS - post_event_countdown
+                            time_since_dissappearance = self.POST_EVENT_SECONDS - self.post_event_countdown
                             
                             cv2.putText(annotated_frame, str(time_since_dissappearance), (20, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
                             
                             if time_since_dissappearance <= 0: 
                                 
-                                post_event_flag = False 
+                                self.post_event_flag = False 
                                 
-                                system_recording = False 
+                                self.system_recording = False 
 
-                                email_sent = False 
+                                self.email_sent = False 
 
-                                first_presence = False 
+                                self.first_presence = False 
 
-                                previous_intruder_count = 0
+                                self.previous_intruder_count = 0
+
+                                 # Email Sending -------
+                            
+                                """
+                                1.For our email system, we have already created the email function above the main function.
                                 
-                                recordedVideo.release()
+                                2. The email system will activate only if an intruder is detected. As a result, we follow 
+                                the current if loop, allowing us to have a cooldown period. 
+                                
+                                3. If an intruder constantly appears, this will cause the system to spam ping the user.
+                                
+                                4. To avoid this, we allow the system to wait for the post even timer to finish.
+                                
+                                5. When the email is sent, we will also send a log to the logbook to record intrusion alerts. 
+                                
+                                """
 
-                                frame_buffer.clear()
+                                self.email_system()
+
+                                self.email_sent = True 
+
+                                with open(file_path, 'a') as file: 
+                                    
+                                        file.write(f"Alert Sent At {current_datetime}.\n")
+                                
+                                self.recordedVideo.release()
+
+                                self.frame_buffer.clear()
                 
                                 
 
     
             
-                cv2.putText(result_frame, str(current_datetime), (20, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+                cv2.putText(self.result_frame, str(current_datetime), (20, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
                 
-                cv2.imshow(WINDOW_NAME, result_frame)
+                cv2.imshow(self.WINDOW_NAME, self.result_frame)
 
                 key = cv2.waitKey(1) & 0xFF
                 
@@ -333,15 +304,15 @@ class LiveFeed():
 
                 elif key == ord("s"):
                     
-                    if system_activated:
+                    if self.system_activated:
                         
                         pass
 
                     else:
                         
-                        countdown = time.time() + DURATION
+                        self.countdown = time.time() + self.DURATION
 
-                        system_activated = True 
+                        self.system_activated = True 
                         
                                         
             else:
@@ -349,7 +320,7 @@ class LiveFeed():
                 break
             
 
-        video_path.release()
+        self.video_path.release()
         cv2.destroyAllWindows()
 
         
